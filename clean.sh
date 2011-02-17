7ee723afbe959fa708eb15b9cf84d9c2fee0b061
Indy Nagpal <indy@nagpals.com>
2011-02-17 15:07:30 +1300

    Adding a script to clean out dictionary caches

---
+++ clean.sh
@@ -0,0 +1,6 @@
+rm -rf ~/Library/Preferences/com.apple.DictionaryServices.plist
+rm -rf ~/Library/Preferences/com.apple.Dictionary.plist
+rm -rf ~/Library/Caches/com.apple.DictionaryApp
+rm -rf ~/Library/Caches/com.apple.DictionaryManager
+rm -rf ~/Library/Caches/com.apple.Dictionary
+rm -rf ~/Library/Caches/com.apple.DictionaryServices
